681609f212b8a1329f0644a31fbea9d8f4b01b68
Steven Smith <nullspace000@gmail.com>
2017-05-20 21:43:33 -0700

    make Input.test.js pass

--- Input.test.js
+++ Input.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import Input from '../Input.js';
+import Input from './Input.js';
 
 test('stub', () => {
   // Render an input class instance
+  const params = {
+    class: 'input',
+    type: 'text',
+    id: 'text-id-1',
+    value: 'some text'
+  };
+  const onChange = function() { /* no need to detect changes in test. */ };
   const input = shallow(
-    <Input />
+    <Input params={params} onChange={onChange.bind(this)} />
   );
-  
+
   // TODO: test
-  
+
 });
